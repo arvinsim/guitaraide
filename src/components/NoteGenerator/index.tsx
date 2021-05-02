3bc1e3da94aa741574abbb607e2f0f736cedd3a6
Arthur Vincent Simon <developarvin@gmail.com>
2021-05-02 17:10:25 +0800

    Update React Hook Form

--- src/components/NoteGenerator/index.tsx
+++ src/components/NoteGenerator/index.tsx
@@ -1,30 +1,31 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { getRandomNote } from "../../utils";
+import React, {useState} from "react";
+import {useForm} from "react-hook-form";
+import {getRandomNote} from "../../utils";
 
 export function NoteGenerator() {
-  const [notes, setNotes] = useState<Array<string>>([]);
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data: any) => {
-    let notesList = [];
-    for (let x = 0; x < data.numberOfNotes; x++) {
-      notesList.push(getRandomNote());
-    }
-    setNotes(notesList);
-  };
+    const [notes, setNotes] = useState<Array<string>>([]);
+    const {register, handleSubmit, formState: {errors}} = useForm();
+    const onSubmit = (data: any) => {
+        let notesList = [];
+        for (let x = 0; x < data.numberOfNotes; x++) {
+            notesList.push(getRandomNote());
+        }
+        setNotes(notesList);
+    };
 
-  return (
-    <div>
-      <h1>Note Generator</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input type={"text"} name={"numberOfNotes"} ref={register} />
-        <input type="submit" value="Generate notes" />
-      </form>
-      <div>
-        {notes.map((note) => {
-          return `${note} `;
-        })}
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Note Generator</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type={"text"} {...register('numberOfNotes')}/>
+                {errors.numberOfNotes}
+                <input type="submit" value="Generate notes"/>
+            </form>
+            <div>
+                {notes.map((note) => {
+                    return `${note} `;
+                })}
+            </div>
+        </div>
+    );
 }
--- src/components/NoteRandomizer/index.tsx
+++ src/components/NoteRandomizer/index.tsx
@@ -1,91 +1,85 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { getRandomArrayItem, getRandomNote } from "../../utils";
+import React, {useState} from "react";
+import {useForm} from "react-hook-form";
+import {getRandomArrayItem, getRandomNote} from "../../utils";
 
 export function NoteRandomizer() {
-  const [string, setString] = useState("");
-  const [note, setNote] = useState("");
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data: any) => {
-    setString(getRandomArrayItem(data.strings));
-    setNote(getRandomNote());
-  };
+    const [string, setString] = useState("");
+    const [note, setNote] = useState("");
+    const {register, handleSubmit, formState: {errors}} = useForm();
+    const onSubmit = (data: any) => {
+        setString(getRandomArrayItem(data.strings));
+        setNote(getRandomNote());
+    };
 
-  return (
-    <div>
-      <h1>Note Randomizer</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <h2>Strings to include</h2>
+    return (
         <div>
-          <input
-            type="checkbox"
-            id="strings_1"
-            name="strings"
-            value="1"
-            ref={register}
-          />
-          <label htmlFor="strings_1">1</label>
-        </div>
-        <div>
-          <input
-            type="checkbox"
-            id="strings_2"
-            name="strings"
-            value="2"
-            ref={register}
-          />
-          <label htmlFor="strings_2">2</label>
-        </div>
-        <div>
-          <input
-            type="checkbox"
-            id="strings_3"
-            name="strings"
-            value="3"
-            ref={register}
-          />
-          <label htmlFor="strings_3">3</label>
-        </div>
-        <div>
-          <input
-            type="checkbox"
-            id="strings_4"
-            name="strings"
-            value="4"
-            ref={register}
-          />
-          <label htmlFor="strings_4">4</label>
-        </div>
-        <div>
-          <input
-            type="checkbox"
-            id="strings_5"
-            name="strings"
-            value="5"
-            ref={register}
-          />
-          <label htmlFor="strings_5">5</label>
-        </div>
-        <div>
-          <input
-            type="checkbox"
-            id="strings_6"
-            name="strings"
-            value="6"
-            ref={register}
-          />
-          <label htmlFor="strings_6">6</label>
-        </div>
+            <h1>Note Randomizer</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <h2>Strings to include</h2>
+                <div>
+                    <input
+                        type="checkbox"
+                        id="strings_1"
+                        defaultValue="1"
+                        {...register('strings')}
+                    />
+                    <label htmlFor="strings_1">1</label>
+                </div>
+                <div>
+                    <input
+                        type="checkbox"
+                        id="strings_2"
+                        defaultValue="2"
+                        {...register('strings')}
+                    />
+                    <label htmlFor="strings_2">2</label>
+                </div>
+                <div>
+                    <input
+                        type="checkbox"
+                        id="strings_3"
+                        defaultValue="3"
+                        {...register('strings')}
+                    />
+                    <label htmlFor="strings_3">3</label>
+                </div>
+                <div>
+                    <input
+                        type="checkbox"
+                        id="strings_4"
+                        defaultValue="4"
+                        {...register('strings')}
+                    />
+                    <label htmlFor="strings_4">4</label>
+                </div>
+                <div>
+                    <input
+                        type="checkbox"
+                        id="strings_5"
+                        defaultValue="5"
+                        {...register('strings')}
+                    />
+                    <label htmlFor="strings_5">5</label>
+                </div>
+                <div>
+                    <input
+                        type="checkbox"
+                        id="strings_6"
+                        defaultValue="6"
+                        {...register('strings')}
+                    />
+                    <label htmlFor="strings_6">6</label>
+                </div>
 
-        <input type="submit" value="Show random note" />
-      </form>
+                <input type="submit" value="Show random note"/>
+            </form>
 
-      <br />
+            <br/>
 
-      <div>
-        <div>String - {string}</div>
-        <div>Note - {note}</div>
-      </div>
-    </div>
-  );
+            <div>
+                <div>String - {string}</div>
+                <div>Note - {note}</div>
+            </div>
+        </div>
+    );
 }
